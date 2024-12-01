75dbb43f159bfb86b5df523e41bd3c9c3f8f445e
srihithaV <Srihithavishwanathula@gmail.com>
2024-12-01 21:57:03 +0530

    Time: 1 ms (65.77%), Space: 45.9 MB (82.16%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,42 +1,24 @@
 class Solution {
     public int trap(int[] height) {
-        int n = height.length;
-        int count=0;
-        int pmax[] = prefix(height,n);
-        int smax[]=  suffix(height,n);
-        for(int i=1;i<n-1;i++)
-        {
-            int lb=pmax[i-1];
-            int rb=smax[i+1];
-            int yb=Math.min(lb,rb);
-            int contri = yb-height[i];
-            if(contri >0)
-            {
-                count=count+contri;
-            }
-        }
-        
-       return count; 
-    }
-    public int[] prefix(int height[],int n)
-    {
-        int[] pmax = new int[n];
+        int n=height.length;
+        int pmax[]=new int[n];
+        int smax[]=new int[n];
+        int store=0;
         pmax[0]=height[0];
         for(int i=1;i<n;i++)
         {
             pmax[i]=Math.max(pmax[i-1],height[i]);
         }
-        return pmax;
-    }
-     public int[] suffix(int height[],int n)
-    {
-        int[] smax = new int[n];
         smax[n-1]=height[n-1];
         for(int i=n-2;i>=0;i--)
         {
-            smax[i]=Math.max(smax[i+1],height[i]);
+            smax[i]=Math.max(height[i],smax[i+1]);
         }
-        return smax;
+        for(int i=1;i<n;i++)
+        {
+            store=store+Math.min(pmax[i],smax[i])-height[i];
+        }
+        return store;
     }
     
 }
