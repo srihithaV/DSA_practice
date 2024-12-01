ce80ab862168ac7adffc6625c26fffaaea92f685
srihithaV <Srihithavishwanathula@gmail.com>
2024-12-01 20:51:32 +0530

    Time: 0 ms (100.00%), Space: 42.3 MB (6.23%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -5,13 +5,13 @@ class Solution {
         int high=n-1;
         int mid=0;
         while(mid<=high)
-        {
-            if(nums[mid]==0)
-            {
-                int temp=nums[mid];
-                nums[mid]=nums[low];
-                nums[low]=temp;
-                mid++;
+        {                                              //Logic
+            if(nums[mid]==0)                // mid starts from 0, low =0, high=n-1
+            {                             // low will store zeroes(left) and high will store two's(right)
+                int temp=nums[mid];       // mid travels from low to <=high
+                nums[mid]=nums[low];  //if mid==0, swaps with low pointer index value and increments the low & mid 
+                nums[low]=temp;       //if mid==1, then skip it(mid++)
+                mid++;                  // if mid==2, swap it with high index value and decreemnt the high pointer
                 low++;
             }
             else if(nums[mid]==1)
