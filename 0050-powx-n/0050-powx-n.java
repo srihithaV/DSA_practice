796fdd4f3e5011d739abd6061f45c04167a34bb8
srihithaV <Srihithavishwanathula@gmail.com>
2025-04-03 18:55:50 +0530

    Time: 0 ms (100%), Space: 43.4 MB (15.49%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -2,10 +2,12 @@ class Solution {
     public double myPow(double x, int n) {
         double res=1.0;
         long pow=n;
+        int set=0;
         if(pow<0)
         {
             pow=pow*(-1);
-            x=1/x;
+            //x=1/x;
+             set=1;
         }
 
         while(pow>0)
@@ -21,6 +23,8 @@ class Solution {
                 pow--;
             }
         }
+        if(set==1)
+        return 1/res;
         return res;
     }
 }
