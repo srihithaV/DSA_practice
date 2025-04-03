a04d2ff16a157860a11d6e32890ff9484a76be30
srihithaV <Srihithavishwanathula@gmail.com>
2025-04-03 18:05:20 +0530

    Time: 0 ms (100%), Space: 40.9 MB (34.58%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.java
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.java
@@ -1,22 +1,11 @@
 class Solution {
     public int trailingZeroes(int n) {
-        int nof=0;
-        int not=0;
-        for(int i=1;i<=n;i++)
+        int count=0;
+        while(n>0)
         {
-            int num=i;
-            while(num%5==0)
-            {
-                nof++;
-                num=num/5;
-            }
-            while(num%2==0)
-            {
-                not++;
-                num=num/2;
-            }
-
+            n=n/5;
+            count=count+n;
         }
-        return Math.min(nof,not);
+        return count;
     }
 }
