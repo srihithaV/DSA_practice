919b2b099f289281f4bcbbeeffeefca9fec2a2e8
srihithaV <Srihithavishwanathula@gmail.com>
2025-04-14 18:00:28 +0530

    Time: 3 ms (73.92%), Space: 43 MB (83.73%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,30 +1,20 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        s=s.trim();
         s=s.toLowerCase();
-        char[] arr=s.toCharArray();
-        int st=0;
-        int ed=arr.length-1;
-        while(st<ed)
-        {
-           while(st<ed && !(arr[st]>='a' && arr[st]<='z' || arr[st]>='A' && arr[st]<='Z'|| arr[st]>='0' && arr[st]<='9'))
-            {
-                st++;
-            }
-          while(st<ed && !(arr[ed]>='a' && arr[ed]<='z' || arr[ed]>='A' && arr[ed]<='Z'||arr[ed]>='0' && arr[ed]<='9'))
-                 {
-                     ed--;
-                 }
-            
-            if(arr[st]==arr[ed])
-            {
-                st++;
-                ed--;
-            }
-            else if(arr[st]!=arr[ed])
-                return false;
-            }
-             return true;
-        }
-       
+        int left=0;
+        int right=s.length()-1;
+        while(left<right)
+         {
+            while(left<right && !(Character.isLetterOrDigit(s.charAt(left))))
+            left++;
+             while(left<right && !(Character.isLetterOrDigit(s.charAt(right))))
+            right--;
+            if(s.charAt(left)!=s.charAt(right))
+            return false;
+            left++;
+            right--;
+
+         } 
+         return true;
     }
+}
